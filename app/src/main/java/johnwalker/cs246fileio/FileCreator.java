a84550835efcb3745e4ca9a2ebc718afeaff4bfd
JohnWalker <wal13024@byui.edu>
2016-05-28 12:25:36 -0600

    linked buttons to functions

---
+++ app/src/main/java/johnwalker/cs246fileio/FileCreator.java
@@ -0,0 +1,17 @@
+package johnwalker.cs246fileio;
+
+/**
+ * Created by johnathon on 5/28/2016.
+ */
+
+class FileCreator implements Runnable {
+    private String filename;
+    FileCreator(String $filename) {
+        filename = $filename;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- app/src/main/java/johnwalker/cs246fileio/MainActivity.java
+++ app/src/main/java/johnwalker/cs246fileio/MainActivity.java
@@ -2,13 +2,27 @@ package johnwalker.cs246fileio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String FILENAME = "numbers.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // hello. please delete me later!
+    }
+
+    public void create(View view) {
+        System.out.println("create buttton working!");
+    }
+
+    public void load(View view) {
+        System.out.println("load buttton working!");
+    }
+
+    public void clear(View view) {
+        System.out.println("clear buttton working!");
     }
 }
